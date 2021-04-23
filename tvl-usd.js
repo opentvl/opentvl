ca7cccfc277ef410d681cb324563486dbd9c9ab4
jtlemkin <jtlemkin@gmail.com>
2021-04-23 13:18:08 -0700

    removed extra BigNumber import

--- tvl-usd.js
+++ tvl-usd.js
@@ -2,7 +2,6 @@ const BigNumber = require('bignumber.js');
 const Bottleneck = require("bottleneck");
 const sdk = require("./sdk");
 const fetch = require("node-fetch");
-const BigNumber = require('bignumber.js');
 const limiter = new Bottleneck({ maxConcurrent: 1, minTime: 600 });
 const debug = require("debug")("opentvl:tvl-usd");
 
