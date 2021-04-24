c73841985c33dd9241da1af2a1b0df15a3fcde66
Zhenhua Cao <zhenhua.cao@certik.org>
2021-04-24 03:12:34 -0400

    add script to update token list for 3 supported chains

--- projects/safemoon/index.js
+++ projects/safemoon/index.js
@@ -1,5 +1,5 @@
-const sdk = require('../../sdk');
-const BigNumber = require('bignumber.js');
+const sdk = require("../../sdk");
+const BigNumber = require("bignumber.js");
 
 /*
   The SafeMoon protocol defines a coin that has a 10% transaction fee.
@@ -11,49 +11,52 @@ const BigNumber = require('bignumber.js');
   The protocol holds onto the LP tokens
 */
 
-const SAFE_MOON_LP = '0x9adc6fb78cefa07e13e9294f150c1e8c1dd566c0';
-const LOCKER = '0xc8b839b9226965caf1d9fc1551588aaf553a7be6';
+const SAFE_MOON_LP = "0x9adc6fb78cefa07e13e9294f150c1e8c1dd566c0";
+const LOCKER = "0xc8b839b9226965caf1d9fc1551588aaf553a7be6";
 
 async function getLPBalance(block) {
-  return (await sdk.bsc.abi.call({
-    target: SAFE_MOON_LP,
-    abi: "bep20:balanceOf",
-    block, 
-    params: LOCKER
-  })).output;
+  return (
+    await sdk.bsc.abi.call({
+      target: SAFE_MOON_LP,
+      abi: "bep20:balanceOf",
+      block,
+      params: LOCKER
+    })
+  ).output;
 }
 
 async function getTotalLPSupply(block) {
-  return (await sdk.bsc.abi.call({
-    target: SAFE_MOON_LP,
-    abi: "bep20:totalSupply",
-    block
-  })).output;
+  return (
+    await sdk.bsc.abi.call({
+      target: SAFE_MOON_LP,
+      abi: "bep20:totalSupply",
+      block
+    })
+  ).output;
 }
 
 async function tvl(_, block) {
-  const lpBalance = await getLPBalance(block);
-  const totalLPSupply = await getTotalLPSupply(block);
-  const percentPoolOwnership = (new BigNumber(lpBalance)).dividedBy(new BigNumber(totalLPSupply));
+  const lpBalance = await getLPBalance(block.bsc);
+  const totalLPSupply = await getTotalLPSupply(block.bsc);
+  const percentPoolOwnership = new BigNumber(lpBalance).dividedBy(new BigNumber(totalLPSupply));
 
   const pairAddresses = [SAFE_MOON_LP];
-  let balances = Object.entries(await sdk.bsc.swap.getReservedBalances(pairAddresses))
-    .reduce((acc, [token, count]) => {
-      const existingBalance = new BigNumber(count);
-      acc[token] = existingBalance.multipliedBy(percentPoolOwnership).toFixed();
-      return acc;
-    }, {});
+  let balances = Object.entries(await sdk.bsc.swap.getReservedBalances(pairAddresses)).reduce((acc, [token, count]) => {
+    const existingBalance = new BigNumber(count);
+    acc[token] = existingBalance.multipliedBy(percentPoolOwnership).toFixed();
+    return acc;
+  }, {});
 
   balances[SAFE_MOON_LP] = lpBalance;
 
-  return (await sdk.bsc.util.toSymbols(balances)).output
+  return (await sdk.bsc.util.toSymbols(balances)).output;
 }
 
 module.exports = {
-  version: '2', // to distinguish new version from old version
-  name: 'Safemoon',
-  token: 'SAFEMOON',
-  category: 'assets',
+  version: "2", // to distinguish new version from old version
+  name: "Safemoon",
+  token: "SAFEMOON",
+  category: "assets",
   start: 1614568378, //Mar-01-2021 03:12:58 AM +UTC
   tvl
-}
+};
--- scripts/updateTokenLists.js
+++ scripts/updateTokenLists.js
@@ -1,22 +1,114 @@
-const sdk = require('../sdk');
-const fs = require('fs');
+const _ = require("underscore");
+const sdk = require("../sdk");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const { parse } = require("node-html-parser");
 
-const BSC_TOKENS = require("../sdk/data/bscTokenLists.json");
-const ETH_TOKENS = require("../sdk/data/ethTokenLists.json");
-const NEW_ADDRESSES = require("../data/bscContractAddresses.json");
+const DECIMALS_ABI = {
+  constant: true,
+  inputs: [],
+  name: "decimals",
+  outputs: [
+    {
+      name: "",
+      type: "uint8"
+    }
+  ],
+  payable: false,
+  stateMutability: "view",
+  type: "function"
+};
 
-async function updateTokenLists() {
-  const bscDecimals = await Promise.all(
-    BSC_TOKENS.map(({ contract }) => sdk.bsc.bep20.decimals(contract))
-  )
+const CHAINS = [
+  {
+    file: "../sdk/data/bscTokenLists.json",
+    scanUrl: "https://bscscan.com",
+    multiCall: sdk.bsc.abi.multiCall
+  },
+  {
+    file: "../sdk/data/ethTokenLists.json",
+    scanUrl: "https://etherscan.io",
+    multiCall: sdk.eth.abi.multiCall
+  },
+  {
+    file: "../sdk/data/hecoTokenLists.json",
+    scanUrl: "https://hecoinfo.com",
+    multiCall: sdk.heco.abi.multiCall
+  }
+];
 
-  const bscTokensWithDecimals = BSC_TOKENS.map(({ symbol, contract }, i) => ({
-    symbol,
-    contract,
-    decimals: parseInt(bscDecimals[i].output.output)
-  }))
-
-  fs.writeFileSync('./data/bscTokenLists.json', JSON.stringify(bscTokensWithDecimals, null, 2))
+function extractSymbol(name) {
+  return name.slice(name.indexOf("(") + 1, name.indexOf(")"));
 }
 
-updateTokenLists();
+function extractAddress(link) {
+  return link.slice(link.indexOf("/token/") + 7);
+}
+
+async function fetchPage(scanUrl, page) {
+  const html = await (await fetch(`${scanUrl}/tokens?ps=100&p=${page}`)).text();
+
+  const root = parse(html);
+  const table = root.querySelector("#tblResult");
+
+  if (!table) {
+    return [];
+  }
+
+  const tableRows = table.querySelectorAll("tbody tr");
+
+  const tokens = tableRows.reduce((acc, row) => {
+    const title = row.querySelector(".text-primary");
+
+    const symbol = extractSymbol(title.innerHTML);
+    const contract = extractAddress(title.getAttribute("href"));
+
+    acc.push({
+      symbol,
+      contract
+    });
+
+    return acc;
+  }, []);
+
+  return tokens;
+}
+
+async function updateTokenLists() {
+  for (let chain of CHAINS) {
+    console.log(`start updating ${chain.file}`);
+
+    // fetch up to 1000 tokens
+    const pageTokens = await Promise.all(_.range(1, 11).map(page => fetchPage(chain.scanUrl, page)));
+
+    const allTokens = pageTokens.flat();
+    const existingTokens = require(chain.file);
+
+    existingTokens.forEach(token => {
+      const hasToken = allTokens.some(t => t.contract === token.contract);
+
+      if (!hasToken) {
+        allTokens.push(token);
+      }
+    });
+
+    const decimals = (
+      await chain.multiCall({ abi: DECIMALS_ABI, calls: allTokens.map(t => ({ target: t.contract })) })
+    ).output.map(o => o.output);
+
+    const updatedTokens = allTokens.reduce((acc, t, idx) => {
+      // ignore tokens with no valid decimal
+      if (decimals[idx]) {
+        acc.push({ ...t, decimals: decimals[idx] });
+      }
+
+      return acc;
+    }, []);
+
+    fs.writeFileSync(chain.file, JSON.stringify(updatedTokens, null, 2));
+
+    console.log(`done updating ${chain.file}, total ${updatedTokens.length} tokens`);
+  }
+}
+
+updateTokenLists().catch(console.error);
